ea4110a2e804b0434498439be58e54dcc75a60ba
Bartek Paczesny <bartek@paczesny.pl>
2024-08-07 21:43:46 +0200

    solves https://linear.app/agencytech/issue/FS-29/configure-development-environment

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,7 @@
+<script lang="ts">
+    import {Button} from "$ui/button";
+</script>
+
 <h1>Home page</h1>
 <p>Here will be the journal selection menu or an option to create a new journal.</p>
+<Button class="mt-5 ml-10">Create a new journal</Button>
